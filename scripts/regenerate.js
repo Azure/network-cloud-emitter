ebef591c1573ce71b05893f884cfa01803c60115
tadelesh <chenjieshi@microsoft.com>
2025-10-27 15:20:24 +0800

    update

--- scripts/regenerate.js
+++ scripts/regenerate.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const projectRoot = path.resolve(__dirname, "..");
-const command = `tsp compile --emit=${projectRoot} --output-dir="${path.join(projectRoot, "test", "generated")}" "${path.join(projectRoot, "test", "spec", "NetworkCloud.Management", "main.tsp")}"`;
+const command = `tsp compile --emit=${projectRoot} --option="network-cloud-emitter.emitter-output-dir=${path.join(projectRoot, "test", "generated")}" "${path.join(projectRoot, "test", "spec", "NetworkCloud.Management", "main.tsp")}"`;
 
 console.log("Running:", command);
 console.log("Project root:", projectRoot);
